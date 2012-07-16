802809aca764883a4503b3c659f281723ebf47d0
appleseed <andromajid@gmail.com>
2012-07-16 20:36:54 +0700

    upload rights

--- protected/config/main.php
+++ protected/config/main.php
@@ -8,11 +8,13 @@ return array(
     'basePath' => dirname(__FILE__) . DIRECTORY_SEPARATOR . '..',
     'name' => 'Task Management',
     // preloading 'log' component
-    'preload' => array('log','bootstrap'),
+    'preload' => array('log', 'bootstrap'),
     // autoloading model and component classes
     'import' => array(
         'application.models.*',
         'application.components.*',
+        'application.modules.rights.*',
+        'application.modules.rights.components.*',
     ),
     'modules' => array('gii' => array(
             'class' => 'system.gii.GiiModule',
@@ -23,15 +25,25 @@ return array(
             // If removed, Gii defaults to localhost only. Edit carefully to taste.
             'ipFilters' => array('127.0.0.1', '::1'),
         ),
+        'rights' => array(
+            'install' => true, // Enables the installer.
+             'superuserName'=>'admin', 
+            'userIdColumn'=>'user_id',
+            'userNameColumn'=>'user_name', 
+        ),
     ),
     // application components
     'components' => array(
         'user' => array(
             // enable cookie-based authentication
+           'class' => 'RWebUser',
             'allowAutoLogin' => true,
         ),
         'bootstrap' => array(
             'class' => 'ext.bootstrap.components.Bootstrap', // assuming you extracted bootstrap under extensions
+        ),
+        'authManager' => array(
+            'class' => 'RDbAuthManager',
         ),
         // uncomment the following to enable URLs in path-format
         'urlManager' => array(
@@ -47,8 +59,9 @@ return array(
             'connectionString' => 'mysql:host=localhost;dbname=task_management',
             'emulatePrepare' => true,
             'username' => 'root',
-            'password' => '',
+            'password' => 'root',
             'charset' => 'utf8',
+            'enableParamLogging'=>true,
         ),
         'errorHandler' => array(
             // use 'site/error' action to display errors
